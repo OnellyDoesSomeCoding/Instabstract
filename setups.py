e039a6a7a1f866bfbf1757938c8f1f40dff0c24b
OnellyDoesSomeCoding <133640626+OnellyDoesSomeCoding@users.noreply.github.com>
2024-12-16 20:56:14 +0700

    Clean up setups.py

--- setups.py
+++ setups.py
@@ -1,12 +1,23 @@
+"""
+This file is the setup options of the main file
+"""
+
 import random
 import turtle
 import shapes
 
 def pensize():
+    """
+    Change the size of pen
+    """
     size = int(input(">>>"))
     turtle.pensize(size)
 
 def setup(speed):
+    """
+    :param speed:
+    Set up overall canvas and set the speed
+    """
     turtle.hideturtle()
     turtle.speed(speed)
     turtle.colormode(255)
@@ -29,12 +40,21 @@ def setup(speed):
 ''')
 
 def reset_screen():
+    """
+    Reset the screen
+    """
     turtle.clear()
 
 def instant():
+    """
+    Finish drawing instantly
+    """
     turtle.tracer(0)
 
 def get_color():
+    """
+    Get parameters for color
+    """
     print("[?] We're currently using RGB Code, The range is between 0 to 255. We shall proceed.")
     while True:
         r = int(input("> R Code : "))
@@ -43,14 +63,18 @@ def get_color():
         if 0 <= r <= 255 and 0 <= g <= 255 and 0 <= b <= 255:
             c = tuple((r, g, b))
             return c
-        else:
-            print("[!] Repeat. We're currently using RGB Code, The range is between 0 to 255.")
+        print("[!] Repeat. We're currently using RGB Code, The range is between 0 to 255.")
 
 def get_parameter(item: str):
+    """
+    :param item:
+    Get other parameters
+    """
     while True:
-        size = int(input(f'''> How big do you want your {item} to be? 0 for random. (Should be about 10 to 60)
+        size = int(input(f'''> How big do you want your {item} to be? 0 for random. \
+        (Should be about 10 to 60)
         Answer : '''))
-        if size < 0:
+        if size >= 0:
             print("[!] Please provide a suitable answer.")
         else:
             break
@@ -63,11 +87,10 @@ def get_parameter(item: str):
         if color == 1:
             color = 0
             break
-        elif color == 2:
+        if color == 2:
             color = get_color()
             break
-        else:
-            print("[!] Please provide a suitable answer.")
+        print("[!] Please provide a suitable answer.")
 
     while True:
         choice_fb = int(input(f'''> Do you want your {item} to be filled?
@@ -77,16 +100,18 @@ def get_parameter(item: str):
         if choice_fb == 1:
             choice_fb = True
             break
-        elif choice_fb == 2:
+        if choice_fb == 2:
             choice_fb = False
             break
-        else:
-            print("[!] Please provide a suitable answer.")
+        print("[!] Please provide a suitable answer.")
 
     return size, color, choice_fb
 
 # Name suggests
 def ask_for_random():
+    """
+    Ask for choices for random patterns
+    """
     while True:
         ball_yes = int(input('''Do you want balls?
         1. Yes
@@ -96,10 +121,9 @@ def ask_for_random():
             ball_size, ball_color, ball_choice = get_parameter("Balls")
             case_ball = shapes.Balls(ball_size, ball_color, ball_choice)
             break
-        elif ball_yes == 2:
+        if ball_yes == 2:
             break
-        else:
-            print("[!] Please provide a suitable answer.")
+        print("[!] Please provide a suitable answer.")
 
     while True:
         poly_yes = int(input('''Do you want polygons?
@@ -112,14 +136,12 @@ def ask_for_random():
                 sides = int(input("> How many sides do you want? 0 for random.\n        Answer : "))
                 if sides >= 3 or sides == 0:
                     break
-                else:
-                    print("[!] Please provide a suitable answer.")
+                print("[!] Please provide a suitable answer.")
             case_poly = shapes.Polygons(poly_size, sides, poly_color, poly_choice)
             break
-        elif poly_yes == 2:
+        if poly_yes == 2:
             break
-        else:
-            print("[!] Please provide a suitable answer.")
+        print("[!] Please provide a suitable answer.")
 
     while True:
         random_amount = int(input("How many shapes do you want? 0 for random. \
@@ -127,11 +149,10 @@ def ask_for_random():
         if random_amount == 0:
             times = random.randint(100,300)
             break
-        elif random_amount > 0:
+        if random_amount > 0:
             times = random_amount
             break
-        else:
-            print("[!] Please provide a suitable answer.")
+        print("[!] Please provide a suitable answer.")
 
     if ball_yes == 1 and poly_yes == 2:
         case_ball.mass_draw(times)
@@ -143,6 +164,9 @@ def ask_for_random():
 
 # Name suggests
 def ask_for_spiral():
+    """
+    Ask for choices for spiral patterns
+    """
     while True:
         what_shape = int(input('''What shapes do you want as your spiral?
         1. Balls
@@ -152,21 +176,19 @@ def ask_for_spiral():
             size, color, filled = get_parameter("Balls")
             spiral_ball = shapes.Spiral(size, color, filled, 0)
             break
-        elif what_shape == 2:
+        if what_shape == 2:
             size, color, filled = get_parameter("Polygons")
             while True:
                 sides = int(input("> How many sides do you want? 0 for random.\n        Answer : "))
                 if sides >= 3:
                     break
-                elif sides == 0:
+                if sides == 0:
                     sides = random.randint(3,6)
                     break
-                else:
-                    print("[!] Please provide a suitable answer.")
+                print("[!] Please provide a suitable answer.")
             spiral_poly = shapes.Spiral(size, color, filled, sides)
             break
-        else:
-            print("[!] Please provide a suitable answer.")
+        print("[!] Please provide a suitable answer.")
 
     while True:
         random_amount = int(input("How many shapes do you want? 0 for random. \
@@ -177,8 +199,7 @@ def ask_for_spiral():
         if random_amount > 0:
             times = random_amount
             break
-        else:
-            print("[!] Please provide a suitable answer.")
+        print("[!] Please provide a suitable answer.")
 
     while True:
         random_angle = int(input("What angle increment do you want? 0 for random. \
@@ -186,11 +207,10 @@ def ask_for_spiral():
         if random_angle == 0:
             angle_inc = random.randint(15,90)
             break
-        elif random_angle > 0:
+        if random_angle > 0:
             angle_inc = random_angle
             break
-        else:
-            print("[!] Please provide a suitable answer.")
+        print("[!] Please provide a suitable answer.")
 
     while True:
         random_distance = int(input("How much distance do you want? 0 for random. \
@@ -198,11 +218,10 @@ def ask_for_spiral():
         if random_distance == 0:
             distance_inc = random.randint(3,5)
             break
-        elif random_distance > 0:
+        if random_distance > 0:
             distance_inc = random_distance
             break
-        else:
-            print("[!] Please provide a suitable answer.")
+        print("[!] Please provide a suitable answer.")
 
     if what_shape == 1:
         spiral_ball.draw_ball_spiral(times, angle_inc, distance_inc)
@@ -211,6 +230,9 @@ def ask_for_spiral():
 
 # Name suggests
 def ask_for_firework():
+    """
+    Ask for choices for fireworks
+    """
     while True:
         color = int(input('''> Do you want the colors to be randomized?
         1. Yes
@@ -219,11 +241,10 @@ def ask_for_firework():
         if color == 1:
             color = 0
             break
-        elif color == 2:
+        if color == 2:
             color = get_color()
             break
-        else:
-            print("[!] Please provide a suitable answer.")
+        print("[!] Please provide a suitable answer.")
 
     while True:
         strands = int(input('''> How many strands do you want? 0 for random.
@@ -231,10 +252,9 @@ def ask_for_firework():
         if strands == 0:
             strands = random.randint(8,15)
             break
-        elif strands > 0:
+        if strands > 0:
             break
-        else:
-            print("[!] Please provide a suitable answer.")
+        print("[!] Please provide a suitable answer.")
 
     while True:
         length = int(input('''> How long do you want the strands to be? 0 for random.
@@ -242,10 +262,9 @@ def ask_for_firework():
         if length == 0:
             length = random.randint(8,15)
             break
-        elif length > 0:
+        if length > 0:
             break
-        else:
-            print("[!] Please provide a suitable answer.")
+        print("[!] Please provide a suitable answer.")
 
     while True:
         amount = int(input('''> How many fireworks do you want? 0 for random.
@@ -253,12 +272,11 @@ def ask_for_firework():
         if amount == 0:
             amount = random.randint(4,8)
             break
-        elif amount > 0:
+        if amount > 0:
             break
-        else:
-            print("[!] Please provide a suitable answer.")
+        print("[!] Please provide a suitable answer.")
 
-    for i in range(amount):
+    for _ in range(amount):
         fire = shapes.Firework(color, strands)
         fire.random_location()
         fire.create_random(length)
