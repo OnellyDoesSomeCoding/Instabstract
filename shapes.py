e923d8b8d728cd3a0c34b82af3a7d1a7be6116a9
OnellyDoesSomeCoding <133640626+OnellyDoesSomeCoding@users.noreply.github.com>
2024-12-16 20:56:40 +0700

    Clean up shapes.py

--- shapes.py
+++ shapes.py
@@ -1,19 +1,38 @@
+"""
+This file contains 4 objects;
+Balls, Polygons, Spiral, Firework
+"""
+
 import turtle
 import random
 import math
 
 
 class Balls:
+    """
+    Mutable Balls Object for drawing balls.
+    """
     def __init__(self, size: int, color, filled: bool):
+        """
+        :param size:
+        :param color:
+        :param filled:
+        """
         self.size = size
         self.color = color if color != 0 else "random"
         self.filled = filled
-        self.location = (0,0)
+        self.location = (0, 0)
 
     def random_location(self):
+        """
+        Randomize Location
+        """
         self.location = (random.randint(-900, 900), random.randint(-500, 500))
 
     def random_color(self):
+        """
+        Randomize Color
+        """
         self.color = (
             random.randint(0, 255),
             random.randint(0, 255),
@@ -21,9 +40,15 @@ class Balls:
         )
 
     def random_size(self):
+        """
+        Randomize Size
+        """
         self.size = random.randint(10, 60)
 
     def draw(self):
+        """
+        Draw a ball
+        """
         if self.color == "random":
             self.random_color()
         turtle.penup()
@@ -38,6 +63,9 @@ class Balls:
             turtle.end_fill()
 
     def mass_draw(self, times):
+        """
+        Draw a number of balls
+        """
         if self.size == 0 and self.color != "random":
             for i in range(times):
                 self.random_size()
@@ -55,8 +83,18 @@ class Balls:
                 self.random_location()
                 self.draw()
 
+
 class Polygons:
+    """
+    Mutable Polygons Object for drawing polygons.
+    """
     def __init__(self, size: int, sides, color, filled: bool):
+        """
+        :param size:
+        :param sides:
+        :param color:
+        :param filled:
+        """
         self.size = size
         self.sides = sides
         self.orient = 0
@@ -65,22 +103,41 @@ class Polygons:
         self.location = (0, 0)
 
     def random_sides(self):
+        """
+        Randomize Sides
+        """
         self.sides = random.randint(3, 6)
 
     def random_orientation(self):
+        """
+        Randomize Orientation
+        """
         self.orient = random.randint(0, 90)
         return self.orient
 
     def random_location(self):
+        """
+        Randomize Location
+        """
         self.location = (random.randint(-900, 900), random.randint(-500, 500))
 
     def random_color(self):
-        self.color = (random.randint(0, 255),random.randint(0, 255),random.randint(0, 255))
+        """
+        Randomize Color
+        """
+        self.color = (random.randint(0, 255), random.randint(
+            0, 255), random.randint(0, 255))
 
     def random_size(self):
+        """
+        Randomize Size
+        """
         self.size = random.randint(10, 60)
 
     def crude_draw(self):
+        """
+        Draw a polygon without moving locations
+        """
         turtle.setheading(self.orient)
         turtle.color(self.color)
         if self.filled:
@@ -92,6 +149,9 @@ class Polygons:
         turtle.end_fill()
 
     def draw(self):
+        """
+        Draw a polygon
+        """
         turtle.penup()
         turtle.goto(self.location[0], self.location[1])
         turtle.setheading(self.orient)
@@ -108,23 +168,26 @@ class Polygons:
         turtle.color(self.color)
 
     def mass_draw(self, times):
+        """
+        Draw a number of polygons
+        """
         if self.sides == 0:
             if self.size == 0 and self.color != "random":
-                for i in range(times):
+                for _ in range(times):
                     self.random_sides()
                     self.random_size()
                     self.random_location()
                     self.random_orientation()
                     self.draw()
             elif self.color == "random" and self.size != 0:
-                for i in range(times):
+                for _ in range(times):
                     self.random_sides()
                     self.random_color()
                     self.random_location()
                     self.random_orientation()
                     self.draw()
             elif self.size == 0 and self.color == "random":
-                for i in range(times):
+                for _ in range(times):
                     self.random_sides()
                     self.random_size()
                     self.random_color()
@@ -133,32 +196,46 @@ class Polygons:
                     self.draw()
         elif self.sides != 0:
             if self.size == 0 and self.color != "random":
-                for i in range(times):
+                for _ in range(times):
                     self.random_size()
                     self.random_location()
                     self.random_orientation()
                     self.draw()
             elif self.color == "random" and self.size != 0:
-                for i in range(times):
+                for _ in range(times):
                     self.random_color()
                     self.random_location()
                     self.random_orientation()
                     self.draw()
             elif self.size == 0 and self.color == "random":
-                for i in range(times):
+                for _ in range(times):
                     self.random_size()
                     self.random_color()
                     self.random_location()
                     self.random_orientation()
                     self.draw()
 
+
 class Spiral(Polygons):
-    def __init__(self, size: int, color, filled: bool, sides = 4):
+    """
+    Spiral object that is made up of polygons and balls
+    """
+    def __init__(self, size: int, color, filled: bool, sides=4):
+        """
+        :param size:
+        :param color:
+        :param filled:
+        :param sides:
+        """
         super().__init__(size, sides, color, filled)
         self.orient = 0
 
     def draw_ball_goto(self, x, y):
-
+        """
+        :param x:
+        :param y:
+        Draw a ball at a spot
+        """
         turtle.penup()
         turtle.goto(x, y - self.size)
         turtle.pendown()
@@ -172,7 +249,11 @@ class Spiral(Polygons):
             turtle.end_fill()
 
     def draw_poly_goto(self, x, y):
-
+        """
+        :param x:
+        :param y:
+        Draw a polygon at a spot
+        """
         turtle.penup()
         turtle.goto(x, y - self.size)
         turtle.setheading(self.orient)
@@ -189,6 +270,12 @@ class Spiral(Polygons):
             turtle.end_fill()
 
     def draw_ball_spiral(self, times, angle_increment, distance_increment):
+        """
+        :param times:
+        :param angle_increment:
+        :param distance_increment:
+        Draw a spiral of balls
+        """
         if self.size == 0 and self.color != "random":
             for i in range(times):
                 self.random_size()
@@ -216,6 +303,12 @@ class Spiral(Polygons):
                 self.draw_ball_goto(x, y)
 
     def draw_poly_spiral(self, times, angle_increment, distance_increment):
+        """
+        :param times:
+        :param angle_increment:
+        :param distance_increment:
+        Draw a spiral of balls
+        """
         if self.size == 0 and self.color != "random":
             for i in range(times):
                 self.random_size()
@@ -303,28 +396,50 @@ class Spiral(Polygons):
                     y = self.location[1] + distance * math.sin(angle)
                     self.draw_poly_goto(x, y)
 
+
 class Firework:
+    """
+    Firework object for one fire work only
+    """
     def __init__(self, color, strands):
+        """
+        :param color:
+        :param strands:
+        """
         self.location = (0, 0)
         self.color = color
         self.strands = strands
 
     def random_color(self):
-        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        """
+        Randomize Color
+        """
+        self.color = (random.randint(0, 255), random.randint(
+            0, 255), random.randint(0, 255))
         turtle.color(self.color)
 
     def goto_location(self):
+        """
+        Go to a location
+        """
         turtle.penup()
         turtle.goto(self.location)
         turtle.pendown()
 
     def random_location(self):
+        """
+        Randomize Location
+        """
         x1, y1 = turtle.screensize()
         x2 = -x1
         y2 = -y1
         self.location = (random.randint(x2, x1), random.randint(y2, y1))
 
     def create_random(self, length):
+        """
+        :param length:
+        Create a random firework
+        """
         random_colors = self.color == 0
 
         for _ in range(self.strands):
@@ -336,6 +451,11 @@ class Firework:
             self.create_branching_strand(random_colors, length)
 
     def create_branching_strand(self, random_colors, length):
+        """
+        :param random_colors:
+        :param length:
+        Create a branch of a firework
+        """
         for _ in range(random.randint(2, 3)):
             turtle.penup()
             turtle.goto(turtle.xcor(), turtle.ycor())
